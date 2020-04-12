1f4bc38a42ce2c5e55aab196c663fa33475bb7c4
Gourisetty <pranithkumar.gourisetty@arcadis.com>
2020-04-12 08:46:46 +0530

    Advanced select Challenges

--- hackerRank-Challenges.sql
+++ hackerRank-Challenges.sql
@@ -1,8 +1,8 @@
 
-
 --- Hacker Rank Challenges
 
---Problem Statement
+-- Basic SQL
+
 -- 1. Revising the Select Query I
 Select * from CITY 
 where POPULATION > 100000 
@@ -109,3 +109,83 @@ From Employee
 where salary > 2000
 AND months < 10
 order by name asc;
+
+-- Advanced SQL
+
+-- 21. Type of Triangle
+Select Case
+		  when a+b > c and b+c > a and c+a > b then
+			  CASE
+				 When a =b and b =c and c = a then "Equilateral"
+				 when a!=b and b!=c and c!=a then "Scalene"
+				 else "Isosceles"
+			  END
+		   Else "Not a triangle"
+		END
+From Triangles
+
+-- 22. The PADS
+
+Select CONCAT(name,"(",LEFT(Name,0),")")
+From OCCUPATIONS
+order by name asc
+
+Select CONCAT("There are a total of", count(*), lower(OCCUPATION) ,".s")
+From OCCUPATIONS 
+Group by OCCUPATION
+order by count(*), occupation asc
+
+-- 23. Binary Tree Nodes
+select N, 
+	if(P is null,'Root',
+	        if((select count(*) from bst where P = B.N)>0,'Inner','Leaf'))
+from BST as B
+order by N
+
+-- 23. New Companies
+
+select c.company_code,c.founder,
+       count(distinct(lm.lead_manager_code)),
+       count(distinct(sm.senior_manager_code)),
+       count(distinct(m.manager_code)),
+	   count(distinct(e.employee_code)) 
+From
+    company c,
+    lead_manager lm,
+    senior_manager sm,
+    manager m,
+    employee e
+where
+	c.company_code = lm.company_code and
+	lm.lead_manager_code = sm.lead_manager_code and
+	sm.senior_manager_code = m.senior_manager_code and
+	m.manager_code = e.manager_code
+group by c.company_code,c.founder
+order by c.company_code
+
+
+-- 24. Occupations
+
+select Doctor,Professor,Singer,Actor
+From
+(Select Name, occupation from Occupations)t
+PIVOT
+(max(Name) for occupation IN (Doctor,Professor,Singer,Actor)) as t2;
+
+
+set @r1=0, @r2=0, @r3=0, @r4=0;
+select min(Doctor), min(Professor), min(Singer), min(Actor)
+from(
+  select case when Occupation='Doctor' then (@r1:=@r1+1)
+            when Occupation='Professor' then (@r2:=@r2+1)
+            when Occupation='Singer' then (@r3:=@r3+1)
+            when Occupation='Actor' then (@r4:=@r4+1) end as RowNumber,
+    case when Occupation='Doctor' then Name end as Doctor,
+    case when Occupation='Professor' then Name end as Professor,
+    case when Occupation='Singer' then Name end as Singer,
+    case when Occupation='Actor' then Name end as Actor
+  from OCCUPATIONS
+  order by Name
+) Temp
+group by RowNumber
+
