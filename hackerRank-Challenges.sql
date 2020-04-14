c361b55803b3e8cb6bc7da55169bcef8f4743dc9
Gourisetty <pranithkumar.gourisetty@arcadis.com>
2020-04-14 10:41:03 +0530

    basic join sql queries

--- hackerRank-Challenges.sql
+++ hackerRank-Challenges.sql
@@ -90,13 +90,11 @@ where city not Rlike '^[aeiou].*'
 AND
 city not Rlike '.*[aeiou]$'
 
-
 -- 18. Higher Than 75 Marks
 select Name
 from STUDENTS
 where marks > 75
 order by RIGHT(Name,3), ID asc;
-
 
 -- 19. Employee Names
 select name
@@ -142,7 +140,7 @@ select N,
 from BST as B
 order by N
 
--- 23. New Companies
+-- 24. New Companies
 
 select c.company_code,c.founder,
        count(distinct(lm.lead_manager_code)),
@@ -164,7 +162,7 @@ group by c.company_code,c.founder
 order by c.company_code
 
 
--- 24. Occupations
+-- 25. Occupations
 --SQL server
 select Doctor,Professor,Singer,Actor
 From
@@ -192,79 +190,170 @@ group by RowNumber
 
 ---- Aggregration
 
---25. Revising Aggregations - The Count Function
+--26. Revising Aggregations - The Count Function
 Select count(Name)
 From CITY
 Where Population > 100000
 
---26. Revising Aggregations - The Sum Function
+--27. Revising Aggregations - The Sum Function
 select sum(POPULATION) from CITY where DISTRICT = 'California'
 
---27. Revising Aggregations - Averages
+--28. Revising Aggregations - Averages
 select avg(Population) from City where District = 'California'
 
---28. Average Population
+--29. Average Population
 select ROUND(avg(Population)) from City
 
---29. Japan Population
+--30. Japan Population
 select sum(Population) from City where COUNTRYCODE = 'jpn'
 
---30. Population Density Difference
+--31. Population Density Difference
 select max(Population)-min(Population) from City
 
---31. The Blunder
+--32. The Blunder
 select CEIL(avg(salary) - avg(replace(salary,'0',''))) from employees
 
---32. Top Earners
+--33. Top Earners
 Select max(salary*months) as earnings,count(*)
 From Employee
 group by earnings 
 order by earnings desc 
 Limit 1
 
---33. Weather Observation Station 2
+--34. Weather Observation Station 2
 Select round(sum(lat_n),2),round(sum(long_w),2)
 From Station
 
---34. Weather Observation Station 13
+--35. Weather Observation Station 13
 Select truncate(sum(lat_n),4)
 From Station
 where lat_n > 38.7880
 and lat_n < 137.2345
 
---35. Weather Observation Station 14
+--36. Weather Observation Station 14
 Select truncate(max(lat_n),4)
 From Station
 where lat_n < 137.2345
 
---36. Weather Observation Station 15
+--37. Weather Observation Station 15
 Select round(long_w,4)
 From Station
 Where lat_n = (select max(lat_n) from station where lat_n < 137.2345)
 
---37. Weather Observation Station 16
+--38. Weather Observation Station 16
 Select round(min(lat_n),4)
 From Station
 where lat_n > 38.7780
 
---38. Weather Observation Station 17
+--39. Weather Observation Station 17
 Select round(long_w,4)
 from station
 where lat_n = (select min(lat_n) from station where lat_n>38.7780)
 
---39. Weather Observation Station 18
+--40. Weather Observation Station 18
 Select round((max(lat_n)-min(lat_n)) + (max(long_w) - min(long_w)),4) 
 from station
 
---40. Weather Observation Station 19
+--41. Weather Observation Station 19
 Select truncate(sqrt(power(max(lat_n)-min(lat_n),2) + power(max(long_w) - min(long_w),2)),4) 
 from station
 
---41. Weather Observation Station 20
+--42. Weather Observation Station 20
 select round(lat_n,4) as median 
 From station s
 where (select count(lat_n) from station where s.lat_n>lat_n) =
  (select count(lat_n) from station where s.lat_n<lat_n)
 
+ ---- Basic Join
 
+ --43. Asian Population
+ select sum(c.population)
+ From city as c
+ Inner Join country ct
+ ON c.CountryCode = ct.code
+ where ct.continent = 'Asia'
 
+--44. African Cities
+Select a.name
+From city a
+Inner Join Country b
+ON a.CountryCode = b.Code
+Where b.Continent = 'Africa'
+
+--45. Average Population of Each Continent
+Select b.continent,floor(AVG(a.population))
+from city a
+Inner Join country b
+ON a.countrycode = b.code
+group by b.continent
+
+--46. The Report
+Select if(g.grade>=8,s.Name,Null), g.grade, s.Marks
+From Students s
+Inner Join Grades g
+ON s.marks between g.min_mark and g.max_mark
+Order by g.grade desc, Name asc,s.marks
+
+--47. Contest Leaderboard
+Select h.hacker_id,h.name, sum(new.ms) as hs
+From hackers h
+inner join 
+	(Select hacker_id, max(score) as ms
+	From submissions
+	group by hacker_id,challenge_id) new
+ON h.hacker_id = new.hacker_id
+group by h.hacker_id,h.name
+having hs > 0
+order by hs desc, h.hacker_id
+
+--48. Top Competitors
+select h.hacker_id,h.name,count(*)
+from submissions s
+Inner Join challenges c
+ON s.challenge_id = c.challenge_id
+Inner join difficulty d
+ON c.difficulty_level = d.difficulty_level
+Inner join hackers h
+ON h.hacker_id = s.hacker_id
+where s.score = d.score and c.difficulty_level = d.difficulty_level
+group by h.hacker_id,h.name
+having count(*) > 1
+order by count(*) desc, h.hacker_id asc
+
+--49. Ollivander's Inventory
+select a.id, b.age, a.coins_needed, a.power 
+from Wands a 
+inner join Wands_Property b 
+on a.code=b.code 
+where b.is_evil != 1 
+and a.coins_needed=(select min(Wands.coins_needed) 
+                    from Wands 
+                    inner join Wands_Property 
+                    on Wands.code=Wands_Property.code 
+                    where Wands_Property.age=b.age 
+                    and Wands.power=a.power) 
+order by a.power desc,b.age desc
+
+--50. Challenges
+
+select H.hacker_id, H.name, count(*) as total
+from Hackers H, Challenges C
+where H.hacker_id = C.hacker_id
+group by H.hacker_id, H.name
+having total = 
+    (select count(*)
+     from challenges
+     group by hacker_id 
+     order by count(*) desc limit 1
+     )
+or total in
+    (select total
+     from (
+        select count(*) as total
+        from Hackers H, Challenges C
+        where H.hacker_id = C.hacker_id
+        group by H.hacker_id, H.name
+      ) counts
+     group by total
+     having count(*) = 1)
+order by total desc, H.hacker_id asc
