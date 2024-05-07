ff205655cf4ef0abcdeab7aab635779deb7d819b
unknown <pranithgourishetty@gmail.com>
2024-05-07 12:43:56 +0530

    joins

---
+++ Custom_sort.sql
@@ -0,0 +1,66 @@
+
+use Practice
+
+---Data table
+create table Happiness_index(
+Rank int,
+Country varchar(50),
+Happiness_2021 NUMERIC(3,2),
+Happiness_2022 NUMERIC(3,2),
+Popultion NUMERIC(15));
+
+--custom sort, order Sweden first
+select * ,
+       CASE when country = 'Sweden' then '1'
+	        else '2' 
+		End as cntry_pr
+from Happiness_index
+order by cntry_pr,Rank asc
+
+
+-- calculating running cost
+-- creates wrong running cost when same amount exists for multiple records
+select *,
+  sum(amount) over(order by amount asc) as cum_cum
+from transactions
+
+-- to solve above, use another col in order or unbounded
+select *,
+   sum(amount) over(order by amount asc rows between unbounded preceding and current row) as cum_sum
+from transactions
+
+
+------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------
+
+select * from int_orders
+
+-- Calculate the sum of amount by sales id (grouping by sales id)
+select *,
+sum(amount) over(partition by salesperson_id)
+from int_orders
+
+-- calculate the rolling sum/ cum sum of amount
+select *,
+sum(amount) over(order by salesperson_id)
+from int_orders
+
+-- calculate the rolling sum by considering above 2 records only
+select *,
+sum(amount) over(order by order_date rows between 2 preceding and current row) as cumsum_2
+from int_orders
+
+
+--- Calculating the cumsum/rolling sum for each sales id
+select *,
+sum(amount) over(partition by salesperson_id order by order_date)
+from int_orders
+
+-- creating lead and lag col
+select *,
+sum(amount) over(order by salesperson_id rows between 1 preceding and 1 preceding) as lag,
+sum(amount) over(order by salesperson_id rows between 1 following and 1 following) as lead
+from int_orders
+
+
+
--- sql_joins.sql
+++ sql_joins.sql
@@ -39,7 +39,7 @@ create table mode
 id int
 );
 
-insert into mode values (1),(2),(2),(3),(3),(3),(3),(4),(5);
+insert into mode values (1),(2),(2),(3),(3),(4),(4),(5);
 
 -- Method 1 - CTE
 with cte_freq as 
@@ -59,6 +59,44 @@ select * from rk_cte where rk=1
 
 
 
+----- Employees whos status is changed
+create table emp_2020
+(
+emp_id int,
+designation varchar(20)
+);
 
+create table emp_2021
+(
+emp_id int,
+designation varchar(20)
+)
+
+insert into emp_2020 values (1,'Trainee'), (2,'Developer'),(3,'Senior Developer'),(4,'Manager');
+insert into emp_2021 values (1,'Developer'), (2,'Developer'),(3,'Manager'),(5,'Trainee');
+
+-- Solution
+
+select isnull(e0.emp_id, e1.emp_id),
+CASE when e0.designation != e1.designation then 'Promoted'
+     when e0.designation is null then 'New'
+	 else 'Resigned'
+	 End as comment
+from emp_2020 e0
+full outer join emp_2021 e1
+on e0.emp_id = e1.emp_id
+where isnull(e0.designation,'ABC') != isnull(e1.designation,'DEF')
+
+
+
+-- create a duplicate based on column
+with cte_freq as
+(select id from mode group by id having count(*) > 1)
+, rk_cte as
+(select id, RANK() over(order by id) as rk  from cte_freq)
+select c.id, 'DUP ' + CAST(rc.rk as varchar(4)) rk_char
+from rk_cte rc
+right join mode c
+on rc.id = c.id
 
 
